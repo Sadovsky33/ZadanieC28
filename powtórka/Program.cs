580a775ab04f7398d2d507b93476c0ddadb67db1
sigma <166521734+Sigmamalefemaleskibidirizzroilet@users.noreply.github.com>
2024-04-15 13:37:55 +0200

    połaczenie plikow

--- powtórka/Program.cs
+++ powtórka/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using powtórka;
+
+class PlikGłówny
+{
+    static void Main(String[] args)
+    {
+        Tablice tablica = new Tablice();
+
+        tablica.Wyswietl();
+    }
+
+}
---
+++ powtórka/Tablice.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace powtórka
+{
+    public class Tablice
+    {
+        public void Wyswietl()
+        {
+            Console.WriteLine("111");
+        }
+    }
+}
