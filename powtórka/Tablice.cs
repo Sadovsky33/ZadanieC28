8808d48fad56ab6744d2761784a5304c7b2091d1
sigma <166521734+Sigmamalefemaleskibidirizzroilet@users.noreply.github.com>
2024-04-15 13:57:28 +0200

    koniec na dzis

--- powtórka/Tablice.cs
+++ powtórka/Tablice.cs
@@ -12,5 +12,20 @@ namespace powtórka
         {
             Console.WriteLine("111");
         }
+
+
+        public void Tworzenie_tablicy()
+        {
+            Console.WriteLine("ile znaków ma mieć tablica?");
+            int dl = int.Parse(Console.ReadLine());
+
+            int[] ints = new int[dl];
+
+            for (int i = 0; i < dl; i++)
+            {
+                Console.WriteLine("podaj kolejny element:");
+                ints[i] = int.Parse(Console.ReadLine()) ;
+            }
+        }
     }
 }
